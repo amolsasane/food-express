09208b34e62bbeaa0847a2ecabdda780f7a9342e
amolsasane <amolsasane001@gmail.com>
2024-09-30 20:12:00 +0530

    style: responsive header

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,59 +35,66 @@ function Header() {
   }, [cartItems.length]);
 
   return (
-    <div className="h-[10vh] fixed bg-white shadow-lg block z-30 w-full slide-down">
+    <div className="h-[10vh] fixed bg-white shadow-lg z-30 w-full slide-down">
       <div className="flex justify-between items-center max-w-[80rem] pl-4 m-auto">
         <div className="img-container flex items-center">
           <Link to="/">
-            <h1 className="logo font-bold text-3xl text-orange-600">
+            <h1 className="logo font-bold text-2xl md:text-3xl text-orange-600 mb-2 md: md:mb-0">
               food<span className="text-black">express</span>
             </h1>
           </Link>
         </div>
 
-        <nav className="">
-          <ul className="flex p-4">
-            <li className="p-2 mx-6 font-bold text-md text-orange-600">
+        <nav>
+          <ul className="flex p-4 space-x-4 sm:space-x-8 md:space-x-12 text-sm md:text-[1rem]">
+            <li className="p-2 font-bold text-orange-600">
               <NavLink
                 to="/"
                 className={({ isActive }) =>
                   isActive ? "text-orange-600" : "text-gray-600"
                 }
               >
-                <FontAwesomeIcon icon={faHouse} />
-                <span className="pl-2 hover:text-orange-600">Home</span>
+                <div className="flex flex-col items-center md:flex-row">
+                  <FontAwesomeIcon icon={faHouse} />
+                  <span className="pl-2 hover:text-orange-600">Home</span>
+                </div>
               </NavLink>
             </li>
-            <li className="p-2 mx-6 font-bold text-md text-orange-600">
+            <li className="p-2 font-bold text-orange-600">
               <NavLink
                 to="/offers"
                 className={({ isActive }) =>
                   isActive ? "text-orange-600" : "text-gray-600"
                 }
               >
-                <FontAwesomeIcon icon={faGift} />
-                <span className="pl-2 hover:text-orange-600">Offers</span>
+                <div className="flex flex-col items-center md:flex-row">
+                  <FontAwesomeIcon icon={faGift} />
+                  <span className="pl-2 hover:text-orange-600">Offers</span>
+                </div>
               </NavLink>
             </li>
-            <li className="p-2 mx-6 font-bold text-md text-orange-600">
+            {/* Removed About Us for small devices */}
+            <li className="p-2 font-bold text-orange-600 hidden md:block">
               <NavLink
                 to="/contact"
                 className={({ isActive }) =>
                   isActive ? "text-orange-600" : "text-gray-600"
                 }
               >
-                <FontAwesomeIcon icon={faHeart} />
-                <span className="pl-2 hover:text-orange-600">About Us</span>
+                <div className="flex flex-col items-center md:flex-row">
+                  <FontAwesomeIcon icon={faHeart} />
+                  <span className="pl-2 hover:text-orange-600">About Us</span>
+                </div>
               </NavLink>
             </li>
-            <li className="p-2 mx-6 font-bold text-md text-orange-600">
+            <li className="p-1 md:p-2 font-bold text-orange-600">
               <NavLink
                 to="/cart"
                 className={({ isActive }) =>
                   isActive ? "text-orange-600" : "text-gray-600"
                 }
               >
-                <div className="flex items-center">
+                <div className="flex flex-col items-center md:flex-row">
                   <div className={`relative ${bounce ? "bounce-down" : ""}`}>
                     <FontAwesomeIcon icon={faCartShopping} />
                     {cartItems.length > 0 && (
@@ -105,18 +112,18 @@ function Header() {
           </ul>
         </nav>
 
-        <div className="relative group flex flex-col mt-2">
-          <button className="mx-6 font-bold text-md text-orange-600">
+        <div className="relative group">
+          <button className="mx-6 font-bold text-sm md:text-[1rem] flex flex-col items-center md:flex-row text-orange-600">
             {loggedInUser === "Guest" ? (
               <FontAwesomeIcon icon={faUser} />
             ) : (
               "Hello,"
             )}
-            <span className="pl-2 text-gray-600 hover:text-orange-600">
+            <span className="md:pl-2 pl-0 text-gray-600 hover:text-orange-600">
               {loggedInUser}
             </span>
           </button>
-          <div className="login-btn text-center text-md bg-gray-200 rounded-md py-2 px-6 absolute mt-7 mx-3 shadow-lg shadow-gray-400 opacity-0 transform translate-y-[-20px] group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-300 ease-in-out">
+          <div className="login-btn text-center text-sm md:text-[1rem] bg-gray-200 rounded-md py-2 px-6 absolute mt-1 md:mt-2 mx-0 md:mx-3 shadow-lg shadow-gray-400 opacity-0 transform translate-y-[-20px] group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-300 ease-in-out">
             <button
               onClick={loginBtnAction}
               className="text-gray-600 font-semibold"
