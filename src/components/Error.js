1cf15f493093a7c494ea5f6e4d1a765d81cb5244
amolsasane <amolsasane001@gmail.com>
2024-10-01 12:06:47 +0530

    fix: error page

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,13 +1,17 @@
 import { useRouteError } from "react-router-dom";
+import errorImg from "../utils/images/err.svg";
 
 const Error = () => {
   const err = useRouteError();
   console.log(err);
   return (
-    <div>
-      <h1>Oops..</h1>
-      <h2>Something went wrong!</h2>
-      <h3>
+    <div className="flex justify-center items-center flex-col pt-[8rem]">
+      <img alt="error" src={errorImg} className="w-[20rem] md:w-[30rem]" />
+      <h1 className="text-red-500 font-bold text-center">Oops..</h1>
+      <h2 className="text-gray-500 font-semibold text-center">
+        Something went wrong, please reload!
+      </h2>
+      <h3 className="text-sm text-gray-500 text-center">
         {err.status}: {err.statusText}
       </h3>
     </div>
