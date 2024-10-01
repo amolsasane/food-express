629249105bb6a93dac9afa3bd12b92a3b3ad65fa
amolsasane <amolsasane001@gmail.com>
2024-10-01 13:43:18 +0530

    style: responsive loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -4,7 +4,11 @@ import loader from "../utils/images/loader.gif";
 const Loader = () => {
   return (
     <div className="flex flex-col justify-center items-center fixed inset-0 w-[80%] m-auto">
-      <img alt="loader" src={loader} className="w-[20%] mt-[10rem]" />
+      <img
+        alt="loader"
+        src={loader}
+        className="md:w-[20%] w-[30%] mt-[10rem]"
+      />
       <div className="w-full h-1 bg-gray-900 bg-gradient-to-r from-white via-gray-400 to-white"></div>
     </div>
   );
