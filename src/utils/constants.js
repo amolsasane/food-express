3fc191254ee6579a283f6a9f70be89d91a0cb6b5
amolsasane <amolsasane001@gmail.com>
2024-10-04 14:12:56 +0530

    fix: removed proxy prefix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,10 +2,10 @@ export const RES_LOGO_LINK =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const RES_MENU_API =
-  "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5744018&lng=73.818406&restaurantId=";
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5744018&lng=73.818406&restaurantId=";
 
 export const SWIGGY_API =
-  "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5744018&lng=73.818406&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5744018&lng=73.818406&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export const MENU_ITEM_API =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/";
