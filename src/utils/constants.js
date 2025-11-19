651d9e3ae35de99df22010d3fcc095b297e96592
amolsasane <amolsasane001@gmail.com>
2025-11-19 15:57:07 +0530

    fix: proxy replaced to corsfix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -45,4 +45,4 @@ export const RASGULLA_LINK =
 export const BURGER_LINK =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/8f508de7-e0ac-4ba8-b54d-def9db98959e_burger.png";
 
-export const PROXY_URL = "https://proxy.cors.sh/";
+export const PROXY_URL = "https://proxy.corsfix.com/?";
